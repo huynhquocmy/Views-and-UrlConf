32d5fe8716eb3a78fa6134915272e4136d552d2d
my huynh <my.huynh@htklabs.com>
2012-05-05 08:55:38 +0700

    First

---
+++ Chapter3_View_Urlconf/Chapter3_View_Urlconf/Chapter3_View_Urlconf/views.py
@@ -0,0 +1,11 @@
+from django.http import Http404, HttpResponse
+import datetime
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
